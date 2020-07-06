6f5e483cd67adf2bb5ccbe39dfbe344502bf6b77
ksoltysiak <krzysztof.soltysiak27@gmail.com>
2020-07-06 23:16:56 +0200

    Templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'KS',
+        'title': 'First post',
+        'content': 'First post content',
+        'date_posted': '06.07.2020'
+    },
+    {
+        'author': 'KS',
+        'title': 'Second post',
+        'content': 'Second post content',
+        'date_posted': '06.07.2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
